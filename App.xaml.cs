945614b745de59b068760bbf99c107ebea5edf3f
GeodesyRnD <53894686+geodesit@users.noreply.github.com>
2024-02-29 09:58:49 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DubaProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Note: it is not best practice to store API keys in source code.
+            // The API key is referenced here for the convenience of this tutorial.
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "AAPK392a480cd4054496b7c9332cd55aeec9-OrUEbhhiZYFRGnF3EZymodr8n23Sn9F2vBrM0Nb0w-zFukCMgp3oRMHEup9OnQB";
+        }
+    }
+
+}
---
+++ GlobalVariebles.cs
@@ -0,0 +1,23 @@
+﻿using DubaProject.Objects;
+
+namespace DubaProject
+{
+    public static class GlobalVariebles
+    {
+        /// <summary>
+        /// Global Variebles class
+        /// </summary>
+        public static string? pathToMobileMapPackage { get; set; }
+        public static Point? PUPoint { get; set; }
+        public static Point? UserPoint { get; set; }
+        public static List<Point> UserPoints { get; set; } = new List<Point>();
+        public static List<Point>? DubaPoints { get; set; } = new List<Point>();
+        public static Point? dubaPoint { get; set; }
+        public static bool PUclicked { get; set; }
+        public static bool PUentered { get; set; }
+        public static bool running { get; set; } = false;
+        public static bool connected { get; set; } = true;
+        public static bool UserPointClicked { get; set; } = false;
+
+    }
+}
